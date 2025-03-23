a5d8da90a7f533d808e403ea3c40118f63e1cc63
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2025-03-23 22:10:40 +0530

    Add /name, /msg endpoints to return application name

---
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/Appcontroller.java
@@ -0,0 +1,21 @@
+package lk.ac.vau.fas.ict.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class Appcontroller {
+	@GetMapping("/msg")
+	public String myMessage() {
+	    return "Hello Springboot";
+	}
+	@GetMapping("/name")
+	public String myName() {
+	    return "My Name is Springboot";
+	}
+
+	
+
+}
